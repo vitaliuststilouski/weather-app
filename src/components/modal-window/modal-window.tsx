a064cb0d3b908af21ce4f3ad371d41c84d1baaf6
Ustilovsky <v.ustilovsky@itransition.com>
2020-09-07 10:18:34 +0300

    add: add styles to the modal-window

--- src/components/modal-window/modal-window.tsx
+++ src/components/modal-window/modal-window.tsx
@@ -3,30 +3,20 @@ import React, { Component } from 'react';
 import './modal-window.css';
 
 export class ModalWindow extends Component<any> {
-    constructor(props: any) {
-        super(props);
-        this.state = {};
-    }
-
-    // state = {
-    //     closeModal: true
-    // }
-
-    onClose = (e: any) => {
-        // this.props.showModal = false;
-        this.props.onClose && this.props.onClose(e);
-      };
-
     render() {
         if (!this.props.showModal) {
             return null
         }
 
-        return (<div>
-                    <h2>Modal Window</h2>
-                    <div>{this.props.children}</div>
-                    <div>
-                        <button onClick={this.props.onClose}>Close</button>
+        return (<div className="modal-window">
+                    <div className="modal-window-wrap">
+                        <h2 className="modal-window-title">Modal Window</h2>
+                        <p className="content">
+                            Lorem ipsum dolor sit amet, consectetur 
+                        </p>
+                        <div>
+                            <button className="close-btn" onClick={this.props.onClose}>Close</button>
+                        </div>
                     </div>
                 </div>)
     }
