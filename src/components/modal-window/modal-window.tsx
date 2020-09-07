f7ee6af107834214bae856ecf9066b7b6e952bb0
Ustilovsky <v.ustilovsky@itransition.com>
2020-09-07 18:43:41 +0300

    add/develop: add styles develop modal-window

--- src/components/modal-window/modal-window.tsx
+++ src/components/modal-window/modal-window.tsx
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
+import { Autocomplete } from '../autocomplete/autocomplete';
 
 import './modal-window.css';
 
 export class ModalWindow extends Component<any> {
+    onAddCity = (e: any) => {
+        e.preventDefault();
+        console.log('AA')
+    }
+
     render() {
         if (!this.props.showModal) {
             return null
         }
 
         return (<div className="modal-window">
-                    <div className="modal-window-wrap">
-                        <h2 className="modal-window-title">Modal Window</h2>
-                        <p className="content">
-                            Lorem ipsum dolor sit amet, consectetur 
-                        </p>
+                    <form className="modal-window-wrap">
+                        <h2 className="modal-window-title">Select City</h2>
+                        <Autocomplete         options={[
+                                "Papaya",
+                                "Persimmon",
+                                "Paw Paw",
+                                "Prickly Pear",
+                                "Peach",
+                                "Pomegranate",
+                                "Pineapple"]}
+                                />
                         <div>
+                            <button className="choose-btn" onClick={() => this.onAddCity}> Add City</button>
                             <button className="close-btn" onClick={this.props.onClose}>Close</button>
                         </div>
-                    </div>
+                    </form>
                 </div>)
     }
 }
