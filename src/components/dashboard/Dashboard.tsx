7f517d1a48cfd9e58218325ec7ba5596da4fffc1
Ustilovsky <v.ustilovsky@itransition.com>
2020-09-09 19:22:05 +0300

    implement: develop delete

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,64 +1,94 @@
-import React, { Component } from "react";
-import { Fields } from "../fields/fields";
-import { ModalWindow } from '../modal-window/modal-window';
-import { WeatherService } from '../../services/weather-service/weather-searvice';
+import React, {Component} from "react";
+import {Fields} from "../fields/fields";
+import {ModalWindow} from '../modal-window/modal-window';
+import {WeatherService} from '../../services/weather-service/weather-searvice';
 
 import "./dashboard.css";
 
 export class Dashboard extends Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.onCloseWindow = this.onCloseWindow.bind(this);
-    this.onAddCity = this.onAddCity.bind(this);
-  }
+    constructor(props: any) {
+        super(props);
+        this.onCloseWindow = this.onCloseWindow.bind(this);
+        this.onAddCity = this.onAddCity.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
 
-  state = {
-      showModal: false,
-      cityName: ''
-  }
+    maxId = 0;
 
-  showModalWindow() {
-    this.setState({
-      showModal: true
-    })
-  }
+    state = {
+        showModal: false,
+        cityName: '',
+        cityTemperature: '',
+        cityId: 0,
+        // cityData: [
+        //     {cityName: '', cityTemperature: '', cityId: 0}
+        // ]
+        cityData: [
+            {cityName: 'Minsk', cityTemperature: 12, cityId: 1},
+            {cityName: 'Moscow', cityTemperature: 15, cityId: 2},
+            {cityName: 'Rome', cityTemperature: 10, cityId: 3},
+            {cityName: 'Rome', cityTemperature: 4, cityId: 4}
+        ]
+    }
 
-  onCloseWindow() {
-    this.setState({
-      showModal: !this.state.showModal
-    })
-  }
+    showModalWindow() {
+        this.setState({
+            showModal: true
+        })
+    }
 
-  onAddCity = (e: any) => {
-    e.preventDefault();
-    this.setState((prevState: boolean) => {
-      return { showModal: !prevState}
-    })
+    onCloseWindow() {
+        this.setState({
+            showModal: !this.state.showModal
+        })
+    }
 
-    const weather = new WeatherService();
-    const { cityName } = this.state;
-    weather.getCity(cityName).then((body) => {
-      console.log(body)
-    })
-  }
+    onAddCity = (e: any) => {
+        e.preventDefault();
+        this.setState((prevState: boolean) => {
+            return {showModal: !prevState}
+        })
 
-  onChange = (e: any) => {
-    console.log(e.currentTarget.value);
-    // this.setState(({
-    //   cityName: e.currentTarget.value
-    // }))
-  }
+        const weather = new WeatherService();
+        const {cityName} = this.state;
+        weather.getCity(this.state.cityName)
+            .then((body) => {
+                this.setState({
+                    cityTemperature: body.main.temp,
+                    cityId: ++this.maxId
+                })
+            })
+    }
 
-  render() {
-    return (
-      <div className="dashboard">
-        <h1 className="title">Weather Forecast</h1>
-        <div className="add-btn-wrapper">
-          <button className="add-btn" onClick = {():void => this.showModalWindow()}>Select City</button>
-        </div>
-        <ModalWindow onChange={(e: any) => this.onChange}   onClose={this.onCloseWindow} onAddCity={this.onAddCity} showModal={this.state.showModal}/>
-        {/* <Fields /> */}
-      </div>
-    );
-  }
+    onChange = (e: any) => {
+        this.setState(({
+            cityName: e.currentTarget.value
+        }))
+    }
+
+    onDeleteCity = (id: number) => {
+        this.setState(({ cityData }: {cityData: any}) => {
+            const cityIdx = cityData.findIndex((el: any) => el.cityId === id);
+            const updatedCities = [...cityData.slice(0, cityIdx), ...cityData.slice(cityIdx + 1)];
+
+            return {
+                cityData: updatedCities
+            };
+        })
+    }
+
+    render() {
+        const { cityName, cityTemperature} = this.state.cityData[0];
+        return (
+            <div className="dashboard">
+                <h1 className="title">Weather Forecast</h1>
+                <div className="add-btn-wrapper">
+                    <button className="add-btn" onClick={(): void => this.showModalWindow()}>Select City</button>
+                </div>
+                <ModalWindow onChange={this.onChange} onClose={this.onCloseWindow} onAddCity={this.onAddCity}
+                             showModal={this.state.showModal}/>
+                {cityName ? <Fields city={cityName} cityTemperature={cityTemperature} cityDataList={this.state.cityData} onDeleted = {this.onDeleteCity}/> : null}
+            </div>
+        );
+    }
 }
