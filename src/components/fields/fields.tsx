c5a8b8056675f33e3885b753834cb66449e272b6
Ustilovsky <v.ustilovsky@itransition.com>
2020-09-09 19:22:34 +0300

    change: change array

--- src/components/fields/fields.tsx
+++ src/components/fields/fields.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {Component} from "react";
 
 import "./fields.css";
 
+export class Fields extends Component<any,any>{
 
-const cells: {cell: number}[] = [{cell: 1 }, {cell: 2 }, {cell: 3 }, {cell: 4 }];
 
-export const Fields = (): any => {
-  return cells.map((el: any, i: number) => {
-    return <div key={i}  className="cell">{el.cell}</div>
-  })
+    render() {
+        const { cityDataList } = this.props;
+
+        return cityDataList.map((el: any, i: number) => {
+
+            return (
+                <div key={i} className="cell">
+                    <p>{`City ${el.cityName}`}</p>
+                    <p>{`Current Temperature ${el.cityTemperature}`}</p>
+                    <button onClick={() => this.props.onDeleted(el.cityId)}>
+                        <i className="fa fa-trash" aria-hidden="true"></i>
+                    </button>
+                </div>)
+        })
+    }
 };
