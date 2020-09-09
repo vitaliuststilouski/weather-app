17ad978cdaa9be18aefd442e374fbe87291e3445
Ustilovsky <v.ustilovsky@itransition.com>
2020-09-09 19:23:37 +0300

    change: lift up props

--- src/components/modal-window/modal-window.tsx
+++ src/components/modal-window/modal-window.tsx
@@ -1,35 +1,29 @@
-import React, { Component } from 'react';
-import { Autocomplete } from '../autocomplete/autocomplete';
+import React, {Component} from 'react';
+import {Autocomplete} from '../autocomplete/autocomplete';
 
 import './modal-window.css';
 
 export class ModalWindow extends Component<any> {
-
-
     render() {
-        console.log(this.props)
-
         if (!this.props.showModal) {
             return null
         }
 
         return (<div className="modal-window">
-            <form className="modal-window-wrap" >
-            <h2 className="modal-window-title">Select City</h2>
-                        <Autocomplete onInput={this.props.onChange}        options={[
-                                "Minsk",
-                                "Persimmon",
-                                "Paw Paw",
-                                "Prickly Pear",
-                                "Peach",
-                                "Pomegranate",
-                                "Pineapple"]}
-                                />
-                        <div>
-                            <button className="choose-btn" onClick={this.props.onAddCity}> Add City</button>
-                            <button className="close-btn" onClick={this.props.onClose}>Close</button>
-                        </div>
-                    </form>
-                </div>)
+            <form className="modal-window-wrap">
+                <h2 className="modal-window-title">Select City</h2>
+                <Autocomplete onInput={this.props.onChange} options={[
+                    "Minsk",
+                    "London",
+                    "Moscow",
+                    "Rome",
+                    "Grodno"]}
+                />
+                <div>
+                    <button className="choose-btn" onClick={this.props.onAddCity}> Add City</button>
+                    <button className="close-btn" onClick={this.props.onClose}>Close</button>
+                </div>
+            </form>
+        </div>)
     }
 }
