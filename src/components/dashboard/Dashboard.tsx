48aff157d01ad5f750504ad70a4b4c82725ed499
Ustilovsky <v.ustilovsky@itransition.com>
2020-09-10 12:47:10 +0300

    add/refactor: change code

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,9 +1,15 @@
 import React, {Component} from "react";
-import {Fields} from "../fields/fields";
+import {WeatherList} from "../weather-list/weather-list";
 import {ModalWindow} from '../modal-window/modal-window';
 import {WeatherService} from '../../services/weather-service/weather-searvice';
 
 import "./dashboard.css";
+
+interface IState {
+    showModal: boolean,
+    city: string,
+    cityData: any,
+};
 
 export class Dashboard extends Component<any, any> {
     constructor(props: any) {
@@ -15,20 +21,10 @@ export class Dashboard extends Component<any, any> {
 
     maxId = 0;
 
-    state = {
+    state: IState = {
         showModal: false,
-        cityName: '',
-        cityTemperature: '',
-        cityId: 0,
-        // cityData: [
-        //     {cityName: '', cityTemperature: '', cityId: 0}
-        // ]
-        cityData: [
-            {cityName: 'Minsk', cityTemperature: 12, cityId: 1},
-            {cityName: 'Moscow', cityTemperature: 15, cityId: 2},
-            {cityName: 'Rome', cityTemperature: 10, cityId: 3},
-            {cityName: 'Rome', cityTemperature: 4, cityId: 4}
-        ]
+        city: '',
+        cityData: []
     }
 
     showModalWindow() {
@@ -45,29 +41,44 @@ export class Dashboard extends Component<any, any> {
 
     onAddCity = (e: any) => {
         e.preventDefault();
-        this.setState((prevState: boolean) => {
+        this.setState((prevState: any) => {
+            console.log(prevState)
             return {showModal: !prevState}
         })
 
         const weather = new WeatherService();
-        const {cityName} = this.state;
-        weather.getCity(this.state.cityName)
+        const {city}: {city: string} = this.state;
+
+        weather.getCity(city)
             .then((body) => {
-                this.setState({
+                const newCity = {
+                    cityName: body.name,
                     cityTemperature: body.main.temp,
-                    cityId: ++this.maxId
+                    cityId: this.maxId++
+                };
+
+                this.setState(({cityData}: { cityData: any }) => {
+                    const newCityList = [
+                        ...cityData,
+                        newCity
+                    ];
+
+                    return {
+                        cityData: newCityList
+                    }
+
                 })
             })
     }
 
     onChange = (e: any) => {
         this.setState(({
-            cityName: e.currentTarget.value
+            city: e.currentTarget.value
         }))
     }
 
     onDeleteCity = (id: number) => {
-        this.setState(({ cityData }: {cityData: any}) => {
+        this.setState(({cityData}: { cityData: any }) => {
             const cityIdx = cityData.findIndex((el: any) => el.cityId === id);
             const updatedCities = [...cityData.slice(0, cityIdx), ...cityData.slice(cityIdx + 1)];
 
@@ -78,16 +89,16 @@ export class Dashboard extends Component<any, any> {
     }
 
     render() {
-        const { cityName, cityTemperature} = this.state.cityData[0];
+        const {city, showModal,  cityData} = this.state;
         return (
             <div className="dashboard">
                 <h1 className="title">Weather Forecast</h1>
                 <div className="add-btn-wrapper">
-                    <button className="add-btn" onClick={(): void => this.showModalWindow()}>Select City</button>
+                    <button className="add-btn" onClick={(): void => this.showModalWindow()}>{cityData.length > 0 ? 'Add City' : 'Select City'}</button>
                 </div>
                 <ModalWindow onChange={this.onChange} onClose={this.onCloseWindow} onAddCity={this.onAddCity}
-                             showModal={this.state.showModal}/>
-                {cityName ? <Fields city={cityName} cityTemperature={cityTemperature} cityDataList={this.state.cityData} onDeleted = {this.onDeleteCity}/> : null}
+                             showModal={showModal}/>
+                {city ? <WeatherList cityDataList={cityData} onDeleted={this.onDeleteCity}/> : null}
             </div>
         );
     }
