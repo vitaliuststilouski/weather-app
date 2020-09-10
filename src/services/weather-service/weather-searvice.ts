1c22749aa33a96fdeeb519f9d814562ed2fcf874
Ustilovsky <v.ustilovsky@itransition.com>
2020-09-10 12:50:56 +0300

    add: add typescript

--- src/services/weather-service/weather-searvice.ts
+++ src/services/weather-service/weather-searvice.ts
@@ -1,18 +1,16 @@
 export class WeatherService {
     _apiWeather = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
-    async getWeather(cityName: string) {
-        const response = await fetch(`${this._apiWeather}${cityName}&units=metric&appid=71dbdfddc08bdc78f7a38812a049b3cb`);
-
+    async getWeather(city: string) {
+        const response = await fetch(`${this._apiWeather}${city}&units=metric&appid=71dbdfddc08bdc78f7a38812a049b3cb`);
         if (!response.ok) {
             throw new Error(`Could not fetch, received ${response.status} `)
         }
-        
         return await response.json();
     }
 
-    getCity(cityName: string) {
-        return this.getWeather(cityName)
+    getCity(city: string) {
+        return this.getWeather(city)
     }
 }
 
