e1be9307c307f495475cf89b637febbabbb40698
Ustilovsky <v.ustilovsky@itransition.com>
2020-09-11 17:43:50 +0300

    add: add logic

--- src/components/autocomplete/autocomplete.tsx
+++ src/components/autocomplete/autocomplete.tsx
@@ -1,8 +1,11 @@
 import React, {Component, Fragment} from "react";
+import {APICities} from '../../services/cities-api/cities-api'
+
 
 import './autocomplete.css'
 
-type State = {
+
+interface IsAutocompleteState {
     activeOption: number,
     filteredOptions: string[],
     showOptions: boolean,
@@ -10,17 +13,13 @@ type State = {
 };
 
 export class Autocomplete extends Component<any> {
-    constructor(props: any) {
-        super(props);
-        this.getInput = this.getInput.bind(this);
-    }
-
-    state: State = {
+    state: IsAutocompleteState = {
         activeOption: 0,
         filteredOptions: [],
         showOptions: false,
         userInput: ''
     };
+
 
     getInput = (e: any) => this.props.onInput(e);
 
@@ -52,21 +51,48 @@ export class Autocomplete extends Component<any> {
         })
     };
 
+    formCitiesArray = () => {
+        const apiCities = new APICities();
+
+        apiCities.getItems()
+            .then((body) => {
+                body.map((el: any) => console.log(el.name, el.country ))
+            })
+    }
+
+    Debounce = (fn: any, delay: any) => {
+        let timeoutID: any;
+        return function (...args: any) {
+            if(timeoutID) {
+                clearTimeout(timeoutID)
+            }
+
+            timeoutID = setTimeout(() => {
+                fn(...args)
+            }, delay)
+        }
+    }
+
     onKeyDown = (e: any) => {
         const {activeOption, filteredOptions} = this.state;
 
         if (e.keyCode === 13) {
+            console.log('13');
             this.setState({
                 activeOption: 0,
                 showOptions: false,
                 userInput: filteredOptions[activeOption]
             });
         } else if (e.keyCode === 38) {
+            console.log('38');
+
             if (activeOption === 0) {
                 return null;
             }
             this.setState({activeOption: activeOption - 1});
         } else if (e.keyCode === 40) {
+            console.log('40');
+
             if (activeOption === filteredOptions.length - 1) {
                 return null;
             }
